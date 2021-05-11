adbdf138d977e330a450970d0e9898fb44454f38
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-05-11 20:12:54 +0530

    src: merge backend and frontend changes
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+export const apiEndpoint = `https://uv5iwqvfm5.execute-api.ap-south-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-gg9z0y7i.us.auth0.com', // Auth0 domain
+  clientId: 'mVPy24nbT8M8DJwj17UDeqFmaMLH3yxe', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
