8d6b2c933385aeb1692fcfcc34ccb5b3f6757726
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-05-12 13:44:41 +0530

    createTodo: handle empty todo name
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- backend/src/functions/http/createTodo/handler.ts
+++ backend/src/functions/http/createTodo/handler.ts
@@ -1,28 +1,29 @@
-import "source-map-support/register";
+import 'source-map-support/register'
 
-import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
-import { formatJSONResponse } from "@libs/apiGateway";
-import { middyfy } from "@libs/lambda";
-import schema from "./schema";
-import { createTodo } from "@businessLogic/todos";
-import { parseUserId } from "src/auth/utils";
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway'
+import { formatJSONResponse } from '@libs/apiGateway'
+import { middyfy } from '@libs/lambda'
+import schema from './schema'
+import { createTodo } from '@businessLogic/todos'
+import { parseUserId } from 'src/auth/utils'
 
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
-	event
+  event
 ) => {
-	const authorizationHeader = event.headers.Authorization;
-	const jwtToken = authorizationHeader.split(" ")[1];
+  const authorizationHeader = event.headers.Authorization
+  const jwtToken = authorizationHeader.split(' ')[1]
+  const userId = parseUserId(jwtToken)
 
-	const userId = parseUserId(jwtToken);
+  if (!event.body.name) return formatJSONResponse({}, 400)
 
-	const todo = await createTodo(event.body, userId);
+  const todo = await createTodo(event.body, userId)
 
-	return formatJSONResponse(
-		{
-			item: todo,
-		},
-		201
-	);
-};
+  return formatJSONResponse(
+    {
+      item: todo
+    },
+    201
+  )
+}
 
-export const main = middyfy(handler);
+export const main = middyfy(handler)
